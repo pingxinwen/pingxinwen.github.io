ae5ca20d376f0d651bd5f0b4cfdbc569070bfedf
pingxinwen <60872624+pingxinwen@users.noreply.github.com>
2021-10-18 14:01:39 +0800

    Update webpack.build.js

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -4,7 +4,7 @@ const baseconfig = require('./webpack.config.js')
 module.exports = merge(baseconfig,{
   mode: 'production',
   output: {
-    publicPath: 'https://blog.pingxinwen.com/'
+    publicPath: 'https://blog.pingxinwen.top/'
   },
   optimization: {
     runtimeChunk: 'single'
