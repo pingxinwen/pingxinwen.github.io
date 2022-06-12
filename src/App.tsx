e35cd7d76e2274d76a1141fdc0e7495fd3caad02
pingxinwen <zhangdewen0224@gmail.com>
2022-06-12 11:59:58 +0800

    refactor: seperate route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
 import './style.less';
 import '@icon-park/react/styles/index.css';
 
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MyHeader } from './component/header';
 import MyRoutes from './config/router';
 
-
 export default (): JSX.Element => {
-
-
   return (
     <Fragment>
       <Router>
         <MyHeader />
-        <main className="container-glass">
-          {/* <p>hello</p>
-          <div style={{
-          height:1000,
-          background:'#ccc',
-          backgroundRepeat:'no-repeat',
-          backgroundPosition:'cover'
-        }}></div> */}
-          <MyRoutes />
+        <main className='main'>
+          <div className="container-glass">
+            <MyRoutes />
+          </div>
         </main>
       </Router>
     </Fragment>
-  )
-}
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import mdx from '@mdx-js/rollup';
+import { visualizer } from 'rollup-plugin-visualizer';
+import remarkFrontmatter from 'remark-frontmatter';
+import { remarkMdxFrontmatter } from 'remark-mdx-frontmatter';
+import yaml from '@rollup/plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     mdx({
-      include: './docs/*/*.mdx',
-      providerImportSource: '@mdx-js/react'
+      providerImportSource: '@mdx-js/react',
+      remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     }),
+    yaml({
+      include:'./src/config/task.yaml',
+    }),
+    // should be last
+    visualizer(),
   ],
   resolve: {
     alias: {
-      "react/jsx-runtime": "react/jsx-runtime.js"
-    }
-  }
-})
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+    },
+  },
+});
